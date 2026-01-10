970d2e8b438d1bfe60f4cb8935eee8524ff63536
Emiliia <emiliakalenska@gmail.com>
2026-01-10 18:39:13 +0100

    wrong commit, too sleepy for THIS fs

--- 2048.js
+++ 2048.js
@@ -35,10 +35,10 @@ function setGame() {
         }
     }
 
-    function updateTile(tile, num) {
-        tile.innerText = "";
-        tile.classList.value = ""; // reset classes
-        tile.classList.add("tile");
+function updateTile(tile, num) {
+    tile.innerText = "";
+    tile.classList.value = ""; // reset classes
+    tile.classList.add("tile");
         if (num > 0) {
             tile.innerText = num;
             tile.classList.add("tile-" + num.toString());
@@ -49,3 +49,46 @@ function setGame() {
             tile.classList.add("x8192");
         }
     }
+
+document.addEventListener ("keyup", (e) => {
+    if (e.code == "ArrowLeft") {
+        slideLeft();
+    }
+})
+
+function filterZero(row) {
+    return row.filterZero (num => num != 0);
+}
+
+function slide(row) {
+    // remove all zeros
+    row = filterZero(row); // [2,2,4]
+    // slide left
+    for (let i = 0; i < row.length-1; i++) {
+        if (row[i] == row[i+1]) {
+            row[i] *= 2;
+            row[i+1] = 0;
+            score += row[i];
+        }
+    }
+
+    row = filterZero(row); // [4,0] -> [4]
+        while (row.length < cols) {
+    row.push(0);
+    }
+    return row;
+}
+
+function slideLeft() {
+    for (let r = 0; r < rows; r++) {
+        let row = board[r]; // get each row
+        row = slide(row);
+        board[r] = row;
+    }
+
+    for (let c = 0; c < cols; c++) {
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+    }
+}
